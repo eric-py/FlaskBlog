72bd1846f27b1b2588bbaab43bc5db519b933646
Eric <halakoei@yahoo.com>
2024-11-01 00:28:16 +0330

    views: create login, logout, register route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,46 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+from .forms import LoginForm, RegistrationForm
+from .models import User, db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
     return render_template('blog/index.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            flash('You have successfully logged in.', 'success')
+            return redirect(next_page or url_for('main.home'))
+        else:
+            flash('Login failed. Please check your username and password.', 'danger')
+    return render_template('account/login.html', title='login', form=form)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('account/register.html', title='Register', form=form)
