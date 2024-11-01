5ff4daca437b1a4d8e500f9435c4f0a24b569cd0
Eric <halakoei@yahoo.com>
2024-11-01 13:40:11 +0330

    __init__: mail config

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,10 @@ from .error_handlers import error_handlers
 from flask_login import LoginManager
 from .views import main
 import os
+from flask_mail import Mail
 
 login_manager = LoginManager()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
@@ -19,9 +21,19 @@ def create_app():
        db.create_all()
        User.create_admin()
 
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    
+    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+    
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     os.makedirs(app.config['TEMPLATES_FOLDER'], exist_ok=True)
     os.makedirs(app.config['STATIC_FOLDER'], exist_ok=True)
+
+    mail.init_app(app)
 
     app.register_blueprint(main, url_prefix = '/')
     app.register_blueprint(error_handlers, url_prefix= '/errors/')
