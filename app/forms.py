368e6c8d07a46e502ec0b436f282465c3f0b825e
Eric <halakoei@yahoo.com>
2024-11-04 21:44:19 +0330

    forms: create PostForm

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from .models import User
+from flask_wtf.file import FileAllowed, FileField
+from .models import User, Category
+from flask_ckeditor import CKEditorField
+
 
 # Forms
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Passwied', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
@@ -40,3 +43,15 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = CKEditorField('Content', validators=[DataRequired()])
+    categories = SelectMultipleField('Categories', coerce=int)
+    image = FileField('Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    status = SelectField('Status', choices=[('d', 'Draft'), ('p', 'Published')], validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.categories.choices = [(c.id, c.name) for c in Category.query.order_by('name')]
